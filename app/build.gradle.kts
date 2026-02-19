83036ed964a17b43096a2c004f57f0c0cd356026
Timothée Rey-Vibet <timothee.reyvibet@gmail.com>
2026-02-19 09:46:51 +0100

    Increased minimum Sdk to support map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.timotheervb.fieldops"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
