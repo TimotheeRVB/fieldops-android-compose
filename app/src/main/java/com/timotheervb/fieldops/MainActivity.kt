5c735d0bdf4ea3137289e19dcf7db9270b33be9a
Timothée Rey-Vibet <timothee.reyvibet@gmail.com>
2026-02-23 22:07:36 +0100

    UI of conversation messages

--- app/src/main/java/com/timotheervb/fieldops/MainActivity.kt
+++ app/src/main/java/com/timotheervb/fieldops/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.timotheervb.fieldops
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +18,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,10 +45,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FieldOpsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(
+                        SampleData.conversationSample
                     )
                 }
             }
@@ -46,42 +59,68 @@ data class Message(val author: String, val body: String)
 
 
 @Composable
-fun MessageCard(message: Message, modifier: Modifier = Modifier) {
+fun MessageCard(message: Message) {
     Row(modifier = Modifier.padding(all = 8.dp), verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = painterResource(R.drawable.image1),
             contentDescription = "Profile picture",
             modifier = Modifier
                 .size(40.dp)
+                .border(1.5.dp, colorScheme.primary, CircleShape)
                 .clip(CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(if (isExpanded) colorScheme.primary else colorScheme.surface)
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = "Hello ${message.author}!",
-                modifier = modifier
+                text = message.author, color = colorScheme.secondary, style = typography.titleMedium
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = "Hello ${message.body}",
-                modifier = modifier
-            )
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                color = surfaceColor,
+                shadowElevation = 1.dp,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = typography.bodyMedium
+                )
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Conversation(messages: List<Message>) {
+    LazyColumn() {
+        items(messages) { message ->
+            MessageCard(message)
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "lightMode")
+@Preview(name = "darkMode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewMessageCard() {
     FieldOpsTheme {
-        MessageCard(message = Message("Timo", "Hi ca va ?"))
+        Surface() {
+            MessageCard(message = Message("Timo", "Hi ca va ?"))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    FieldOpsTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
